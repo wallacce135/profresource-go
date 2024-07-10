4893e92ab0057342c91c936940b03b208cc433a3
wallacce135 <wallacce135@gmail.com>
2024-07-10 10:40:59 +0300

    show err

--- env/loadenv.go
+++ env/loadenv.go
@@ -7,9 +7,9 @@ import (
 )
 
 func LoadEnvironments() {
-	err := godotenv.Load()
+	err := godotenv.Load("./.env")
 
 	if err != nil {
-		log.Fatal("Error loading .env file!")
+		log.Fatal("Error loading .env file!", err.Error())
 	}
 }
